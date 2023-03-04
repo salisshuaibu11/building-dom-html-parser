c9f407d17e30818d6ddbe378fed6925cd24b472d
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-03-05 00:48:50 +0100

    struct and function fo instantiation for Node and NodeType

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,56 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+use std::fmt;
+
+struct Node {
+    children: Vec<Node>,
+    node_type: NodeType
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+enum NodeType {
+    Text(String),
+    Element(ElementData),
+    Comment(String)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+struct ElementData {
+    tag_name: String,
+    attributes: AttrMap,
+}
+
+type AttrMap = HashMap<String, String>;
+
+impl Node {
+    fn new(node_type: NodeType, children: Vec<Node>) -> Node {
+        Node {
+            node_type,
+            children
+        }
+    }
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self.node_type)
+    }
+}
+
+impl fmt::Debug for NodeType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            NodeType::Text(ref t) | NodeType::Comment(ref t) => write!(f, "{}", t),
+            NodeType::Element(ref e) => write!(f, "{:?}", e)
+        }
+    }
+}
+
+impl fmt::Debug for ElementData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut attributes_string = String::new();
+
+        for (attr, value) in self.attributes.iter() {
+            attributes_string.push_str(&format!("{}=\"{}\"", attr, value));
+        }
+
+        write!(f, "<{},{}>", self.tag_name, attributes_string)
     }
 }
